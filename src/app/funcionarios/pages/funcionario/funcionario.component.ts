08b24d434bf332c5b13c6befcb267916f3d1e72a
renato3x <pereirarenato21@gmail.com>
2022-06-28 08:46:12 -0300

    recuperando funcionario do banco de dados

--- src/app/funcionarios/pages/funcionario/funcionario.component.ts
+++ src/app/funcionarios/pages/funcionario/funcionario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Funcionario } from '../../models/funcionario';
+import { FuncionarioService } from '../../services/funcionario.service';
 
 @Component({
   selector: 'app-funcionario',
@@ -8,12 +10,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FuncionarioComponent implements OnInit {
 
+  funcionario!: Funcionario
+
   constructor(
-    private route: ActivatedRoute // acessar os parâmetros da rota ativa
+    private route: ActivatedRoute, // acessar os parâmetros da rota ativa
+    private funcService: FuncionarioService
   ) { }
 
   ngOnInit(): void {
-    let idFuncionario = this.route.snapshot.paramMap.get('idFuncionario')
-    console.log(idFuncionario)
+    // let idFuncionario = this.route.snapshot.paramMap.get('idFuncionario')
+
+    this.route.paramMap.subscribe(
+      (params) => {
+        let idFuncionario = parseInt(params.get('idFuncionario') ?? '0')
+        this.recuperarFuncionario(idFuncionario)
+      }
+    )
+  }
+
+  recuperarFuncionario(id: number): void {
+    this.funcService.getFuncionarioById(id)
+    .subscribe(
+      func => {
+        this.funcionario = func
+      }
+    )
   }
 }
