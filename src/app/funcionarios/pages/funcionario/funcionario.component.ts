f1122003c8de822ae9799a2a765fef62c825f5f3
renato3x <pereirarenato21@gmail.com>
2022-06-30 18:00:23 -0300

    visualização do funcionario concluída

--- src/app/funcionarios/pages/funcionario/funcionario.component.ts
+++ src/app/funcionarios/pages/funcionario/funcionario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
 import { Funcionario } from '../../models/funcionario';
 import { FuncionarioService } from '../../services/funcionario.service';
 
@@ -12,14 +14,23 @@ export class FuncionarioComponent implements OnInit {
 
   funcionario!: Funcionario
 
+  formFuncionario: FormGroup = this.fb.group({
+    nome: ['', [ Validators.required ]],
+    email: ['', [ Validators.required, Validators.email ]]
+  })
+
+  imagePreview: string = ''
+  foto!: File
+  desabiltar: boolean = true
+
   constructor(
     private route: ActivatedRoute, // acessar os parâmetros da rota ativa
-    private funcService: FuncionarioService
+    private funcService: FuncionarioService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     // let idFuncionario = this.route.snapshot.paramMap.get('idFuncionario')
-
     this.route.paramMap.subscribe(
       (params) => {
         let idFuncionario = parseInt(params.get('idFuncionario') ?? '0')
@@ -30,9 +41,41 @@ export class FuncionarioComponent implements OnInit {
 
   recuperarFuncionario(id: number): void {
     this.funcService.getFuncionarioById(id)
+    .pipe(
+      tap(func => {
+        this.formFuncionario.setValue({
+          nome: func.nome,
+          email: func.email
+        })
+
+        this.imagePreview = func.foto
+        this.valorMudou()
+      })
+    )
     .subscribe(
       func => {
         this.funcionario = func
+      }
+    )
+  }
+
+  recuperarFoto(event: any): void {
+    this.foto = event.target.files[0]
+
+    const reader = new FileReader()
+
+    reader.readAsDataURL(this.foto)
+
+    reader.onload = () => {
+      this.imagePreview = reader.result as string
+    }
+  }
+
+  valorMudou() {
+    this.formFuncionario.valueChanges
+    .subscribe(
+      ({ nome, email }) => {
+        this.desabiltar = !(nome != this.funcionario.nome || email != this.funcionario.email)
       }
     )
   }
