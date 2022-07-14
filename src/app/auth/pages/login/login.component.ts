73680e4083e9cf29a9520a477138e5aabd0ec953
renato3x <pereirarenato21@gmail.com>
2022-07-14 11:54:54 -0300

    fazendo login e armazendo o token no local storage

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +13,31 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup = this.fb.group({
     login: ['', [ Validators.required ]],
-    password:['', [ Validators.required ]]
+    password: ['', [ Validators.required ]]
   })
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private snackbar: MatSnackBar,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    const credenciais = this.loginForm.value
+
+    this.authService.signIn(credenciais)
+    .subscribe(
+      () => {
+        this.snackbar.open('Logado com sucesso', 'Ok', {
+          duration: 3000
+        })
+
+        this.router.navigateByUrl('/funcionarios')
+      }
+    )
   }
 }
