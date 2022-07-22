4878bfaa915b1c31fdfddc9c04b14a06e6e6d1c6
vitorpfranco <vitor.pereira.franco@gmail.com>
2022-07-22 13:58:10 -0300

    Refatoramento FuncService

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAIh0nUciDJUVb6VLj5nd4m-nKo5OeI2Hk",
-    authDomain: "soulcode-app-services.firebaseapp.com",
-    projectId: "soulcode-app-services",
-    storageBucket: "soulcode-app-services.appspot.com",
-    messagingSenderId: "897999773081",
-    appId: "1:897999773081:web:1dad83b8760a5ebd484708",
-    measurementId: "G-TC182RVTS2"
+    apiKey: "AIzaSyDXRPh2EC2uKlUl2jkvQ3_wB10m9T8BqxI",
+    authDomain: "appservicessoulcode.firebaseapp.com",
+    projectId: "appservicessoulcode",
+    storageBucket: "appservicessoulcode.appspot.com",
+    messagingSenderId: "382416819457",
+    appId: "1:382416819457:web:fbde98652c8a0c05cdb700",
+    measurementId: "G-T4J740MR5E"
   }
 };
 
