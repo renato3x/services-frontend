01eac0bc97cd44597d7421670c62005211b9db3e
RafaelTabolka <rafaeltabolka@hotmail.com>
2022-07-25 14:18:32 -0300

    Adicionado campo idCargo no model de Funcionarios

--- src/app/funcionarios/models/funcionario.ts
+++ src/app/funcionarios/models/funcionario.ts
@@ -1,6 +1,9 @@
+import { Cargos } from "src/app/cargos/interface/cargos"
+
 export interface Funcionario {
   idFuncionario?: number
   nome: string
   email: string
   foto: string
+  cargo:Cargos
 }
