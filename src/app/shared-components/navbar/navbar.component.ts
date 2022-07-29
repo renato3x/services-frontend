74bf7d80d56cdbe0a680eb611c077b831e4219e7
vitorpfranco <vitor.pereira.franco@gmail.com>
2022-07-29 19:00:17 -0300

    Responsive Nav Bar

--- src/app/shared-components/navbar/navbar.component.ts
+++ src/app/shared-components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState, MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { ConfirmarLogoutComponent } from '../confirmar-logout/confirmar-logout.component';
 
@@ -9,34 +11,53 @@ import { ConfirmarLogoutComponent } from '../confirmar-logout/confirmar-logout.c
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+  /* isHandset:Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.isHandset) */
+  private _mobileQueryListener: () => void;
 
-  emailUser!: string;
-
-  constructor(
-    private dialog: MatDialog,
-    public authService: AuthService
-  ) { }
-
-  ngOnInit(): void {
-    this.emailUser= this.emailUsuario()
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private authService: AuthService) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  emailUsuario():string {
-    const email= this.authService.emailUsuario().sub
-    return email
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-  logout() {
-    const dialog= this.dialog.open(ConfirmarLogoutComponent)
-    dialog.afterClosed().subscribe((Response) => {
-      if(Response) {
-        this.authService.signOut()
-      }
-    })
+
+  tempoToken() {
+    return this.authService.tempoApp()
+  }
+
+
+  /* 
+    emailUser!: string;
   
-  }
+    constructor(
+      private dialog: MatDialog,
+      public authService: AuthService
+    ) { }
   
+    ngOnInit(): void {
+      this.emailUser= this.emailUsuario()
+    }
+  
+    emailUsuario():string {
+      const email= this.authService.emailUsuario().sub
+      return email
+    }
+    logout() {
+      const dialog= this.dialog.open(ConfirmarLogoutComponent)
+      dialog.afterClosed().subscribe((Response) => {
+        if(Response) {
+          this.authService.signOut()
+        }
+      })
+    
+    } */
 
-  }
+
+}
 
 
